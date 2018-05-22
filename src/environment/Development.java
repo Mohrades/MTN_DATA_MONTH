bc1999a19e0f7a28ad73e6107fbb303b77c086d8
Sedar <Sedar@MOHRADES.mshome.net>
2018-05-22 11:03:37 +0100

    2018-05-22 11:03 AM Commit

--- src/environment/Development.java
+++ src/environment/Development.java
@@ -93,7 +93,7 @@ public class Development {
 						modele.put("amount", http.getHeaderField("Amount"));
 					}
 					modele.put("next", http.getHeaderField("FreeFlow").equals("FC"));
-					modele.put("message", response.toString());
+					modele.put("message", response.toString().replace("\\n", "\n"));
 
 				} catch (NullPointerException|NumberFormatException e) {
 
